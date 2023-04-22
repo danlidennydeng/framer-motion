5385743480bc99aefd277b9b2c76e9737636a9f2
Danli Denny Deng <danlideng@yahoo.com>
2023-04-22 15:35:52 -0700

    drag with framer-motion

---
+++ pages/drag.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import DragSquare from '../MyComponents/DragSquare'
+
+export default function Drag() {
+  return (
+    <div>
+      <DragSquare />
+    </div>
+  )
+}
