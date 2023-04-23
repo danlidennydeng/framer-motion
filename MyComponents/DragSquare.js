26f7c1424d6ffd31734d98a92815c4f1565760c4
Danli Denny Deng <danlideng@yahoo.com>
2023-04-22 18:21:42 -0700

    touch-to-drag fixed again?

--- MyComponents/DragSquare.js
+++ MyComponents/DragSquare.js
@@ -8,8 +8,8 @@ function DragSquare() {
 
   return (
 
-    <div className={styles.boxSection} ref={parentRef}>
-          <motion.div className={styles.box1} drag dragConstraints={parentRef} whileDrag={{scale: 1.1}} style={{ touchAction: "none" }}>
+    <div className={styles.boxSection} ref={parentRef}  style={{ overflowY: "scroll", WebkitOverflowScrolling: "touch" }}>
+          <motion.div className={styles.box1} drag dragConstraints={parentRef} whileDrag={{scale: 1.1}}>
           
           </motion.div>
         </div>
